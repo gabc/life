2a708617e063fa82a09b39ec559bdd19acf8baa6
Gabriel Beauchamp <beauchampgabriel@gmail.com>
2020-03-30 16:27:38 -0400

    Remove test tests that aren't needed anymore

--- test/life_test.exs
+++ test/life_test.exs
@@ -2,11 +2,6 @@ defmodule LifeTest do
   use ExUnit.Case
   doctest Life
 
-  test "greets the world" do
-    assert Life.hello() == :world
-    assert :foo.bar(0) == 0
-  end
-
   test "the spot is occupied or not" do
     map = [{:pos, 0,0}, {:pos, 1,0}]
     assert :world.is_occupied([], 0) == :false
